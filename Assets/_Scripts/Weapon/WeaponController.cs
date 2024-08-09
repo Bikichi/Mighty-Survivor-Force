3d09194eaabfcaa4de496707553df5f72a1d65ad
PhongNguyen <ngthephong2001@gmail.com>
2024-08-09 13:18:37 +0700

    Player Controller, Controll Camera Basic, Weapon Basic

---
+++ Assets/_Scripts/Weapon/WeaponController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+	public Transform weaponHold;
+	public Weapon startignWeapon;
+	public Weapon equipedWeapon;
+
+	private void Start()
+	{
+		if (startignWeapon != null)
+			EquipWeapon (startignWeapon);
+	}
+
+	public void EquipWeapon(Weapon weapon)
+	{
+		if (equipedWeapon != null)
+			Destroy (equipedWeapon.gameObject);
+
+		equipedWeapon = Instantiate(weapon, weaponHold.position, weaponHold.rotation, weaponHold);
+	}
+
+	public void Shoot()
+	{
+		if (equipedWeapon != null)
+			equipedWeapon.Shoot ();
+	}
+}
