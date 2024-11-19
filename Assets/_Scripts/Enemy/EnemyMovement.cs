c612091da4104547970dff2714d15a95ffa557cf
PC-GreenAcademy <ngthephong2001@gmail.com>
2024-11-19 21:19:34 +0700

    Fix lỗi các Enemy đẩy nhau khi di chuyển...

--- Assets/_Scripts/Enemy/EnemyMovement.cs
+++ Assets/_Scripts/Enemy/EnemyMovement.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,7 +8,17 @@ public class EnemyMovement : MonoBehaviour
     private const string runParaname = "Run";
     public Animator anim;
     public EnemyScriptableObject enemyData;
+    public Rigidbody rb;
 
+    public float distance;
+    public bool isMoving;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.drag = 0f;
+        rb.angularDrag = 0f;
+    }
     void Update()
     {
         MoveEnemy();
@@ -16,16 +26,24 @@ public class EnemyMovement : MonoBehaviour
 
     public void MoveEnemy()
     {
-        if (CheckDistance.Instance.CalculateDistanceFromPlayerToEnemy(_targetPlayer.transform, transform) <= 3.5f)
+        if (CheckDistance.Instance.CalculateDistanceFromPlayerToEnemy(_targetPlayer.transform, transform) <= distance)
         {
             anim.SetBool(runParaname, false);
+            isMoving = false;
             return;
-        } 
+        }
         else
         {
             anim.SetBool(runParaname, true);
+            isMoving = true;
             transform.LookAt(_targetPlayer.transform, Vector3.up);
-            transform.Translate(Vector3.forward * Time.deltaTime * enemyData.enemyMoveSpeed);
+            //transform.Translate(Vector3.forward * Time.deltaTime * enemyData.enemyMoveSpeed);
+            Vector3 direction = (_targetPlayer.transform.position - transform.position).normalized;
+            Vector3 movement = direction * Time.deltaTime * enemyData.enemyMoveSpeed;
+
+            // Di chuyển thông qua Rigidbody và áp dụng một lực nhỏ để tránh va chạm mạnh
+            Vector3 currentVelocity = rb.velocity;
+            rb.velocity = new Vector3(movement.x, currentVelocity.y, movement.z); // Chỉ thay đổi vận tốc trên mặt đất
         }
     }
 }
