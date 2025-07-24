ce2df4a72e161966eadc3a9a2b9247cdc7e0c6b7
PhongNguyen <ngthephong2001@gmail.com>
2025-07-25 03:53:49 +0700

    ...

--- Assets/_Scripts/Unit/Drone/DroneBullet.cs
+++ Assets/_Scripts/Unit/Drone/DroneBullet.cs
@@ -33,27 +33,4 @@ public class DroneBullet : BaseBullet
         transform.Translate(direction * _speedBullet * Time.deltaTime, Space.World);
         transform.forward = direction;
     }
-    private void OnTriggerEnter(Collider col)
-    {
-        if (col.CompareTag(Const.ENEMY_TAG))
-        {
-            EnemyHealth enemyHealth = col.GetComponent<EnemyHealth>();
-            if (enemyHealth != null)
-            {
-                enemyHealth.TakeDamage(damageBullet);
-
-                Vector3 hitPosition = col.ClosestPoint(transform.position); //lấy vị trí va chạm gần nhất
-                Vector3 impactDirection = (col.transform.position - transform.position).normalized; //hướng vật thể va chạm tới Enemy
-
-                if (_hitEffect != null)
-                {
-                    Quaternion hitRotation = Quaternion.LookRotation(-impactDirection); //quay ngược lại hướng va chạm
-                    GameObject effect = Instantiate(_hitEffect, hitPosition, hitRotation); //effect sinh ra với hướng ngược với hướng của vật thể lao tới
-                    Destroy(effect, 1f);
-                }
-
-                Destroy(gameObject, 0.2f);
-            }
-        }
-    }
 }
